51b5f71d2ffde7d0fe4ddaec8a2db5a15bd73e36
DevEzenwa <jesseezenwa911@gmail.com>
2023-02-15 03:46:55 -0800

    updated

--- src/pages/welcome/Welcome.jsx
+++ src/pages/welcome/Welcome.jsx
@@ -3,28 +3,28 @@ import "./welcome.scss";
 import { Link } from "react-router-dom";
 const Welcome = () => {
   return (
-    <div className="welcome">
-      <div className="main">
-        <img className="logo" src="assets/Group.jpg" />
+    <div className='welcome'>
+    <div className='main'>
+        <img className='logo' src="assets/Group.jpg" />
 
-        <div className="background">
-          <img src="assets/pablo-sign-in 1.jpg" />
+        <div className='background'>
+            <img src="assets/pablo-sign-in 1.jpg"/>
         </div>
-      </div>
-
-      <div className="cover">
-        <div className="head">
-          <h1 className="header">Welcome!</h1>
-          <p>Enter details to login</p>
-        </div>
-        <input className="input" type="email" placeholder="Email"></input>
-        <input className="input" type="password" placeholder="Password"></input>
-        <a href="#">forgot password?</a>
-        <Link to="/home">
-          <button className="btn">log in</button>
-        </Link>
-      </div>
     </div>
+
+<div className='cover'>
+    <div className='head'>
+   <h1 className='header'>Welcome!</h1>
+   <p>Enter details to login</p> 
+    </div>
+   <input className='input' type="email" placeholder='Email'></input>
+   <input className='input' type="password" placeholder='Password'></input>
+    <a href='#'>forgot password?</a>
+    <Link to="/home" className="linkbtn">
+      <button className='btn'>log in</button>
+    </Link>
+</div>
+</div>
   );
 };
 export default Welcome;
