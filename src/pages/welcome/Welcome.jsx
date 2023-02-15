9c4b834dea439158ba4b56d25e0bd770c27c0e4b
DevEzenwa <jesseezenwa911@gmail.com>
2023-02-15 04:17:14 -0800

    update

--- src/pages/welcome/Welcome.jsx
+++ src/pages/welcome/Welcome.jsx
@@ -17,13 +17,19 @@ const Welcome = () => {
    <h1 className='header'>Welcome!</h1>
    <p>Enter details to login</p> 
     </div>
-    <div>
+    <div className="body">
+      <div>
    <input className='input' type="email" placeholder='Email'></input>
    <input className='input' type="password" placeholder='Password'></input>
+      </div>
+   <div>
     <a href='#'>forgot password?</a>
+   </div>
+   <div>
     <Link to="/home" className="linkbtn">
       <button className='btn'>log in</button>
     </Link>
+   </div>
     </div>
 </div>
 </div>
